cc626cdf5339141ca5072f336cf95a1fa2ac56fb
Floriana <mongeflorencia94@gmail.com>
2024-09-28 14:06:25 -0300

    #15- Creación de Migration. Agregamos campo DNI a Patient

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae5c3121ceb20ad80f688207e6e176394e1180c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df86f3f83d80af0f799ca8a265cb85561476fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
