9c480b6cd1c35a18b2d81d5bffa193d2224af8d8
Floriana <mongeflorencia94@gmail.com>
2024-09-27 22:01:48 -0300

    #8 - Migraciones de la base de datos

---
+++ Api/Migrations/20240928002235_model_001.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class model_001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Property",
+                columns: table => new
+                {
+                    PropertyID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedUserID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Property", x => x.PropertyID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyValue",
+                columns: table => new
+                {
+                    PropertyValueID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PropertyCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PropertyId = table.Column<long>(type: "bigint", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ValueString = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ValueDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ValueInt = table.Column<int>(type: "int", nullable: true),
+                    ValueFloat = table.Column<float>(type: "real", nullable: true),
+                    TextES = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TextEN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TextPT = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedUserID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyValue", x => x.PropertyValueID);
+                    table.ForeignKey(
+                        name: "FK_PropertyValue_Property_PropertyId",
+                        column: x => x.PropertyId,
+                        principalTable: "Property",
+                        principalColumn: "PropertyID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    PatientID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusId = table.Column<long>(type: "bigint", nullable: false),
+                    HealthInsurance = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedUserID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.PatientID);
+                    table.ForeignKey(
+                        name: "FK_Patient_PropertyValue_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "PropertyValue",
+                        principalColumn: "PropertyValueID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusId = table.Column<long>(type: "bigint", nullable: false),
+                    Administrative = table.Column<bool>(type: "bit", nullable: false),
+                    Doctor = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedUserID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                    table.ForeignKey(
+                        name: "FK_User_PropertyValue_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "PropertyValue",
+                        principalColumn: "PropertyValueID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Token",
+                columns: table => new
+                {
+                    TokenID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    UpdatedAt = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedUserID = table.Column<int>(type: "int", nullable: true),
+                    UpdatedUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Token", x => x.TokenID);
+                    table.ForeignKey(
+                        name: "FK_Token_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patient_StatusId",
+                table: "Patient",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_Code",
+                table: "Property",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyValue_Code",
+                table: "PropertyValue",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyValue_PropertyId",
+                table: "PropertyValue",
+                column: "PropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Token_UserId",
+                table: "Token",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Token_Value",
+                table: "Token",
+                column: "Value",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_StatusId",
+                table: "User",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patient");
+
+            migrationBuilder.DropTable(
+                name: "Token");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "PropertyValue");
+
+            migrationBuilder.DropTable(
+                name: "Property");
+        }
+    }
+}
